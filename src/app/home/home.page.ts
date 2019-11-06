0de6308e8f4587139eab6859c2f0f11f57cffa1b
Antonio manzano <alum.antonio.manzano.borrego@ieslosremendios.org>
2019-11-06 13:41:49 +0100

    jhknnkl

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from  '@angular/common/http';
+import { map }  from  'rxjs/operators';
+ 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  sex: any;
+  users: any;
+  
+  constructor(private httpClient: HttpClient) {
+    this.users = this.httpClient.get('https://raw.githubusercontent.com/antoniomb7265/aplicacionconjson/master/generated.json').pipe(map(res => res['results']));
+  }
+ 
 }
